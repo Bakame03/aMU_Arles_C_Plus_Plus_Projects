31ff17dd4bb723870919fe956095e37fd034f155
bakame03 <nganjialdoalex@gmail.com>
2026-02-03 11:00:24 +0100

    Enhancement_TP1_2_3_Polynome

---
+++ C++/Semester2/TP1_2_3/Polynome.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "Polynome.hpp"
+
+Polynome::Polynome(unsigned int _degre, double _coef[])
+    : degre(_degre), coef(nullptr) {
+    coef = new double[degre + 1];
+    for (unsigned int i = 0; i <= degre; ++i) 
+        coef[i] = _coef[i];
+}
+
+Polynome::~Polynome() {
+    delete[] coef;
+}
+
+// Original copy constructor implementation (From sctratch)
+// Polynome::Polynome(const Polynome& p)
+//     : degre(p.degre), coef(nullptr) {
+//     coef = new double[degre + 1];
+//     for (unsigned int i = 0; i <= degre; ++i) 
+//         coef[i] = p.coef[i];
+// }
+
+// Using delegating constructor to avoid code duplication
+Polynome::Polynome(const Polynome& p)
+    : Polynome(p.degre, p.coef) {
+}
+
+Polynome& Polynome::operator+=(const Polynome& p) {
+    if (p.degre > degre) {
+        double* newCoef = new double[p.degre + 1];
+        for (unsigned int i = 0; i <= degre; i++)
+            newCoef[i] = coef[i] + p.coef[i];
+        for (unsigned int i = degre + 1; i <= p.degre; i++)
+            newCoef[i] = p.coef[i];
+        delete[] coef;
+        coef = newCoef;
+        degre = p.degre;
+    } else {
+        for (unsigned int i = 0; i <= p.degre; i++)
+            coef[i] += p.coef[i];
+    }
+    return *this;
+}
---
+++ C++/Semester2/TP1_2_3/Polynome.hpp
@@ -0,0 +1,18 @@
+#ifndef __POLYNOME_HPP__
+#define __POLYNOME_HPP__
+
+class Polynome {
+private:
+    unsigned int degre;
+    double* coef;
+public:
+    Polynome(unsigned int _degre, double _coef[]);
+    Polynome(const Polynome& p);
+    Polynome() = delete;
+    inline unsigned int getDegre() const { return degre; }
+    ~Polynome();
+    Polynome& operator+=(const Polynome& p);
+    
+};
+
+#endif 
