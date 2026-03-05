eb1bbf5f970fe2b9ff6eb06c6fcc86ce99949788
bakame03 <nganjialdoalex@gmail.com>
2026-03-05 11:09:12 +0100

    IHM

--- R2_02_IHM/TP1/tp1/fenetre.cpp
+++ R2_02_IHM/TP1/tp1/fenetre.cpp
@@ -6,6 +6,10 @@ Fenetre::Fenetre(QWidget *parent)
     setWindowTitle("ConvertDist");
     setWindowFlags(Qt::WindowStaysOnTopHint);
     resize(250,150);
+
+    valeurMetre = new QDoubleSpinBox(this);
+    valeurFoot = new QDoubleSpinBox(this);
+    valeurInch = new QDoubleSpinBox(this);
 }
 
 Fenetre::~Fenetre() {}
