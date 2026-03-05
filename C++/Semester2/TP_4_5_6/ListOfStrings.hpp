d2595065b0930b54b91b3fc83947bf73f257eac3
bakame03 <nganjialdoalex@gmail.com>
2026-03-05 16:07:00 +0100

    refactor: changed `StringNode` class visibility from public to private.

--- C++/Semester2/TP_4_5_6/ListOfStrings.hpp
+++ C++/Semester2/TP_4_5_6/ListOfStrings.hpp
@@ -6,17 +6,16 @@
 
 class ListOfStrings
 {
-    class StringNode
-    {
-        public:
-            std::string value;
-            StringNode* next;
-            StringNode(std::string, StringNode* = nullptr);
-            StringNode(const StringNode&);
-            ~StringNode();
-    };
-
     private:
+        class StringNode
+        {
+            public:
+                std::string value;
+                StringNode* next;
+                StringNode(std::string, StringNode* = nullptr);
+                StringNode(const StringNode&);
+                ~StringNode();
+        };
         size_t count;
         StringNode* first;
         StringNode* last;
